7c8c637bbc932ed91f1dd60d318006e5a3a716f3
Johan Silkens <me@jsilkens.be>
2025-06-05 17:03:54 +0200

    feat(domain-objects) Use factory in main class (#8)
    Co-authored-by: johan <johan@catnco.ovh>

--- Kotlin/gildedrose-app/src/main/kotlin/com/gildedrose/application/TexttestFixture.kt
+++ Kotlin/gildedrose-app/src/main/kotlin/com/gildedrose/application/TexttestFixture.kt
@@ -1,6 +1,6 @@
 package com.gildedrose.application
 
-import com.gildedrose.domain.Item
+import com.gildedrose.domain.factory.createItem
 import com.gildedrose.usecase.GildedRose
 
 fun main(args: Array<String>) {
@@ -8,16 +8,16 @@ fun main(args: Array<String>) {
     println("OMGHAI!")
 
     val items = listOf(
-        Item("+5 Dexterity Vest", 10, 20), //
-            Item("Aged Brie", 2, 0), //
-            Item("Elixir of the Mongoose", 5, 7), //
-            Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-            Item("Sulfuras, Hand of Ragnaros", -1, 80),
-            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        createItem("+5 Dexterity Vest", 10, 20), //
+            createItem("Aged Brie", 2, 0), //
+            createItem("Elixir of the Mongoose", 5, 7), //
+            createItem("Sulfuras, Hand of Ragnaros", 0, 80), //
+            createItem("Sulfuras, Hand of Ragnaros", -1, 80),
+            createItem("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            createItem("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            createItem("Backstage passes to a TAFKAL80ETC concert", 5, 49),
             // this conjured item does not work properly yet
-            Item("Conjured Mana Cake", 3, 6)
+            createItem("Conjured Mana Cake", 3, 6)
     )
 
     val app = GildedRose(items)
