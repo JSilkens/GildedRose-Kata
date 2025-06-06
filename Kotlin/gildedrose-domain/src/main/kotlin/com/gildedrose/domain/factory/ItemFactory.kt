b3df6564e50526a854fd89fb815a7434151539f1
johan <johan@catnco.ovh>
2025-06-06 08:36:03 +0200

    feat(domain-objects) Add backstage pass object

--- Kotlin/gildedrose-domain/src/main/kotlin/com/gildedrose/domain/factory/ItemFactory.kt
+++ Kotlin/gildedrose-domain/src/main/kotlin/com/gildedrose/domain/factory/ItemFactory.kt
@@ -1,6 +1,7 @@
 package com.gildedrose.domain.factory
 
 import com.gildedrose.domain.AgedBrie
+import com.gildedrose.domain.BackstagePass
 import com.gildedrose.domain.Conjured
 import com.gildedrose.domain.Item
 import com.gildedrose.domain.Sulfuras
@@ -10,6 +11,7 @@ fun createItem(name: String , sellin: Int , quality: Int): Item {
         name.contains("Sulfuras", ignoreCase = true) -> Sulfuras(name, sellin, quality)
         name.contains("Aged Brie", ignoreCase = true) -> AgedBrie(name, sellin, quality)
         name.contains("Conjured", ignoreCase = true) -> Conjured(name, sellin, quality)
+        name.contains("Backstage pass" , ignoreCase = true) -> BackstagePass(name, sellin, quality)
         else -> Item(name, sellin, quality)
     }
 }
